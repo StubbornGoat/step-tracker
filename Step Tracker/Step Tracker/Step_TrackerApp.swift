5fc5461f97fa6d5085c5958d0c93825753a7720e
Jack Ziegenhorn <jackziegenhorn@gmail.com>
2024-08-17 17:10:38 -0500

    created xcode project

---
+++ Step Tracker/Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Jack Ziegenhorn on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
